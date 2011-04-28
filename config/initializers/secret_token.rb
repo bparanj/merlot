958c73ffbbd627701a16f91cb99450d2de4c8e91
Bala Paranj <bparanj@gmail.com>
2011-04-28 12:04:26 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merlot::Application.config.secret_token = '4ac463f4da5cbe028450bf6e7cac6c0bf7d151b70c302e1fa8c88c303bf5b130dd61d2f6d336758a4591e5bfba9538f778188cae2d130a859c2e67b34e66d0d9'
