9613a7e06d07276f9f8a5e74230c112a8f638745
Bala Paranj <bparanj@gmail.com>
2011-05-11 22:42:09 -0700

    Tab highlighting implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def is_active?(page_name)
+    # logger.info("lllllllllllll #{page_name}")
+    # logger.info("cccccccccccccc #{params[:controller]}")
+    "current" if params[:controller] == page_name
+  end
+  
 end
