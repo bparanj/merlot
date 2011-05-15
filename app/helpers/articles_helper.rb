5ab467815d69f66a3183344fb3531f4b30abcb7c
Bala Paranj <bparanj@gmail.com>
2011-05-15 03:55:06 -0700

    Refactor article show author and date details to view helper

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+  def author_published_on(article)
+    result = ""
+    result += "By " + article.created_by + " on " if article.created_by     
+    result += article.published_on if article.published_on
+  end
 end
