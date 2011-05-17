8b9d1a7fffadd453219cac2b6cc4a09159682c45
Bala Paranj <bparanj@gmail.com>
2011-05-17 02:22:16 -0700

    Configure mail settings to fix error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,10 @@ Merlot::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'www.creditcardslogic.com' }
+  config.action_mailer.perform_deliveries = true
   
   # Enable threaded mode
   # config.threadsafe!
