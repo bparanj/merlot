8947903acd113faa0c9d91427f07121f6ed3fde5
Bala Paranj <bparanj@gmail.com>
2011-05-30 15:50:09 -0700

    Increase time out value for admin to 60 minutes

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,4 @@
 class Admin < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :timeoutable, :validatable, 
-         :lockable, :trackable, :timeout_in => 20.minutes
+         :lockable, :trackable, :timeout_in => 60.minutes
 end
