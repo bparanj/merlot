2118b9e9138b53c683f8915a5e33ec4d376130cb
Bala Paranj <bparanj@gmail.com>
2011-10-03 08:22:10 -0700

    Generate sitemap only for articles

--- config/initializers/sitemap.rb
+++ config/initializers/sitemap.rb
@@ -1,7 +1,7 @@
 DynamicSitemaps::Sitemap.draw do
   
   # default per_page is 50.000 which is the specified maximum.
-  # per_page 10
+  per_page 10
 
   url root_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 1
   
@@ -11,11 +11,11 @@ DynamicSitemaps::Sitemap.draw do
     url article_url(article), :last_mod => article.updated_at, :change_freq => 'monthly', :priority => 0.8
   end
 
-  new_page!
+  # new_page!
   
-  autogenerate  :articles,
-                :last_mod => :updated_at,
-                :change_freq => 'weekly',
-                :priority => 0.8
+  # autogenerate  :categories,
+  #               :last_mod => :updated_at,
+  #               :change_freq => 'weekly',
+  #               :priority => 0.8
                   
 end
