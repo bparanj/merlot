44d175732edb8ad393fa3344efddd163af8d37fe
Bala Paranj <bparanj@gmail.com>
2011-10-18 16:19:37 -0700

    Comment out peformance code for debugging fb like feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,9 +9,9 @@ class HomeController < ApplicationController
   end
   
   def fb_channel
-    response.headers['Pragma'] = 'public'
-    expires_in 1.year, :public=>true
-    response.headers["Expires"] = CGI.rfc1123_date(Time.now + 1.year)
+    # response.headers['Pragma'] = 'public'
+    # expires_in 1.year, :public=>true
+    # response.headers["Expires"] = CGI.rfc1123_date(Time.now + 1.year)
     render :text=>'<script src="//connect.facebook.net/en_US/all.js"></script>' + "\n"
   end
 end
