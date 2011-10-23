902fd789dd22a109649e854798a669de221463ae
Bala Paranj <bparanj@gmail.com>
2011-10-23 08:19:40 -0700

    Changed feedback email title

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -7,7 +7,7 @@ class FeedbacksController < ApplicationController
     @feedback = Feedback.new(params[:feedback])
     if @feedback.valid?
       Pony.mail(:to => 'bparanj@gmail.com', :from => params[:email], 
-                :subject => 'RadiumLogic Feedback', 
+                :subject => 'Credit Cards Logic Feedback', 
                 :body => "Sent By: #{params[:feedback][:name]} Message is #{params[:feedback][:message]}" )
       flash[:notice] = "Message sent! Thank you for contacting us."
       redirect_to root_url
